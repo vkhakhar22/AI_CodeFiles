f1f6d2c94dd500d556784d26d347ddb898a0be3f
vkhakhar22 <vanshikhakhar22.svkm@gmail.com>
2025-08-30 12:35:08 +0530

    Add files via upload

---
+++ LESSON-1/project.py
@@ -0,0 +1,6 @@
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+if age>=18:
+        print(name,"Congratulations!You are allowed to drive")
+else:
+    print(name,"Sorry not eligible to drive..better luck next time")
